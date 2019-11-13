7aee07e8ac534eef303a77747455f0466ae6e574
Peter Rafaj <peter.rafaj@kistler.com>
2019-11-13 14:52:29 +0100

    clean

--- doc/sensor.c
+++
@@ -1,7 +0,0 @@
-//
-// Created by rafik on 1.4.2019.
-//
-#include "sensor.h"
-
-
-
--- src/main.c
+++ src/main.c
@@ -17,173 +17,102 @@
  *
  ****************************************************************************/
 
-#include "cloud.h"
 #include "oc_api.h"
-#include "oc_core_res.h"
-#include "port/oc_clock.h"
-#include "rd_client.h"
-#include <pthread.h>
 #include <signal.h>
-#include <stdio.h>
+#include <inttypes.h>
 
-// define application specific values.
-static const char *spec_version = "ocf.1.0.0";
-static const char *data_model_version = "ocf.res.1.0.0";
+static int quit;
 
-static const char *resource_rt = "core.light";
-static const char *device_rt = "oic.d.cloudDevice";
-static const char *device_name = "Cloud Device";
+#if defined(_WIN32)
+#include <windows.h>
 
-static const char *manufacturer = "ocfcloud.com";
+static CONDITION_VARIABLE cv;
+static CRITICAL_SECTION cs;
 
-pthread_mutex_t mutex;
-pthread_cond_t cv;
-
-oc_resource_t *res1;
-oc_resource_t *res2;
-
-int quit = 0;
-
-static void cloud_status_handler(cloud_status_t status, void *user_data) {
-    (void)user_data;
-    printf("cloud_status: %d\n", (int)status);
+static void
+signal_event_loop(void)
+{
+  WakeConditionVariable(&cv);
 }
 
-static int app_init(void) {
-    int ret = oc_init_platform(manufacturer, NULL, NULL);
-    ret |= oc_add_device("/oic/d", device_rt, device_name, spec_version,
-                         data_model_version, NULL, NULL);
-    ret |= cloud_init(0, cloud_status_handler, NULL);
-    return ret;
+void
+handle_signal(int signal)
+{
+  signal_event_loop();
+  quit = 1;
 }
 
-struct light_t {
-    bool state;
-    int power;
-};
+static int
+init(void)
+{
+  InitializeCriticalSection(&cs);
+  InitializeConditionVariable(&cv);
 
-struct light_t light1 = {0};
-struct light_t light2 = {0};
+  signal(SIGINT, handle_signal);
+  return 0;
+}
 
-static void get_handler(oc_request_t *request, oc_interface_mask_t interface,
-                        void *user_data) {
-    struct light_t *light = (struct light_t *)user_data;
-
-    printf("get_handler:\n");
-
-    oc_rep_start_root_object();
-    switch (interface) {
-        case OC_IF_BASELINE:
-            oc_process_baseline_interface(request->resource);
-            /* fall through */
-        case OC_IF_RW:
-            oc_rep_set_boolean(root, state, light->state);
-            oc_rep_set_int(root, power, light->power);
-            oc_rep_set_text_string(root, name, "Light");
-            break;
-        default:
-            break;
+static void
+run(void)
+{
+  while (quit != 1) {
+    oc_clock_time_t next_event = oc_main_poll();
+    if (next_event == 0) {
+      SleepConditionVariableCS(&cv, &cs, INFINITE);
+    } else {
+      oc_clock_time_t now = oc_clock_time();
+      if (now < next_event) {
+        SleepConditionVariableCS(
+          &cv, &cs, (DWORD)((next_event - now) * 1000 / OC_CLOCK_SECOND));
+      }
     }
-    oc_rep_end_root_object();
-    oc_send_response(request, OC_STATUS_OK);
+  }
 }
 
-static void post_handler(oc_request_t *request, oc_interface_mask_t iface_mask,
-                         void *user_data) {
-    struct light_t *light = (struct light_t *)user_data;
-    (void)iface_mask;
-    printf("post_handler:\n");
-    oc_rep_t *rep = request->request_payload;
-    while (rep != NULL) {
-        char *key = oc_string(rep->name);
-        printf("key: %s ", key);
-        if (key && !strcmp(key, "state")) {
-            switch (rep->type) {
-                case OC_REP_BOOL:
-                    light->state = rep->value.boolean;
-                    printf("value: %d\n", light->state);
-                    break;
-                default:
-                    oc_send_response(request, OC_STATUS_BAD_REQUEST);
-                    return;
-            }
-        } else if (key && !strcmp(key, "power")) {
-            switch (rep->type) {
-                case OC_REP_INT:
-                    light->power = rep->value.integer;
-                    printf("value: %d\n", light->power);
-                    break;
-                default:
-                    oc_send_response(request, OC_STATUS_BAD_REQUEST);
-                    return;
-            }
-        }
-        rep = rep->next;
-    }
-    oc_send_response(request, OC_STATUS_CHANGED);
-    oc_notify_observers(request->resource);
-}
+#elif defined(__linux__) || defined(__ANDROID_API__)
+#include <pthread.h>
 
-static void register_resources(void) {
-    res1 = oc_new_resource(NULL, "/light/1", 1, 0);
-    oc_resource_bind_resource_type(res1, resource_rt);
-    oc_resource_bind_resource_interface(res1, OC_IF_RW);
-    oc_resource_set_default_interface(res1, OC_IF_RW);
-    oc_resource_set_discoverable(res1, true);
-    oc_resource_set_observable(res1, true);
-    oc_resource_set_request_handler(res1, OC_GET, get_handler, &light1);
-    oc_resource_set_request_handler(res1, OC_POST, post_handler, &light1);
-    cloud_rd_publish(res1);
-    oc_add_resource(res1);
+static pthread_mutex_t mutex;
+static pthread_cond_t cv;
 
-    res2 = oc_new_resource(NULL, "/light/2", 1, 0);
-    oc_resource_bind_resource_type(res2, resource_rt);
-    oc_resource_bind_resource_interface(res2, OC_IF_RW);
-    oc_resource_set_default_interface(res2, OC_IF_RW);
-    oc_resource_set_discoverable(res2, true);
-    oc_resource_set_observable(res2, true);
-    oc_resource_set_request_handler(res2, OC_GET, get_handler, &light2);
-    oc_resource_set_request_handler(res2, OC_POST, post_handler, &light2);
-    cloud_rd_publish(res2);
-    oc_add_resource(res2);
-}
-
-static void signal_event_loop(void) {
+static void
+signal_event_loop(void)
+{
     pthread_mutex_lock(&mutex);
     pthread_cond_signal(&cv);
     pthread_mutex_unlock(&mutex);
 }
 
-void handle_signal(int signal) {
-    (void)signal;
+void
+handle_signal(int signal)
+{
+    if (signal == SIGPIPE) {
+        return;
+    }
     signal_event_loop();
     quit = 1;
 }
 
-int main(void) {
-    int init;
+static int
+init(void)
+{
     struct sigaction sa;
     sigfillset(&sa.sa_mask);
     sa.sa_flags = 0;
     sa.sa_handler = handle_signal;
     sigaction(SIGINT, &sa, NULL);
-
-    static const oc_handler_t handler = {.init = app_init,
-            .signal_event_loop = signal_event_loop,
-            .register_resources =
-            register_resources};
-
-    oc_storage_config("./cloud_linux_creds");
+    sigaction(SIGPIPE, &sa, NULL);
 
     if (pthread_mutex_init(&mutex, NULL) < 0) {
-        printf("pthread_mutex_init failed!\n");
+        OC_ERR("pthread_mutex_init failed!");
         return -1;
     }
+    return 0;
+}
 
-    init = oc_main_init(&handler);
-    if (init < 0)
-        return init;
-
+static void
+run(void)
+{
     while (quit != 1) {
         oc_clock_time_t next_event = oc_main_poll();
         pthread_mutex_lock(&mutex);
@@ -197,8 +126,165 @@ int main(void) {
         }
         pthread_mutex_unlock(&mutex);
     }
+}
 
-    cloud_shutdown(0);
+#else
+#error "Unsupported OS"
+#endif
+
+// define application specific values.
+static const char *spec_version = "ocf.1.0.0";
+static const char *data_model_version = "ocf.res.1.0.0";
+
+static const char *resource_rt = "core.switch.1";
+static const char *device_rt = "oic.d.switch.device";
+static const char *device_name = "Cloud Device";
+
+static const char *manufacturer = "ocfcloud.com";
+
+oc_resource_t *res1;
+
+static void
+cloud_status_handler(oc_cloud_context_t *ctx, oc_cloud_status_t status,
+                     void *data)
+{
+    (void)data;
+    PRINT("\nCloud Manager Status:\n");
+    if (status & OC_CLOUD_REGISTERED) {
+        PRINT("\t\t-Registered\n");
+    }
+    if (status & OC_CLOUD_TOKEN_EXPIRY) {
+        PRINT("\t\t-Token Expiry: ");
+        if (ctx) {
+            PRINT("%d\n", oc_cloud_get_token_expiry(ctx));
+        } else {
+            PRINT("\n");
+        }
+    }
+    if (status & OC_CLOUD_FAILURE) {
+        PRINT("\t\t-Failure\n");
+    }
+    if (status & OC_CLOUD_LOGGED_IN) {
+        PRINT("\t\t-Logged In\n");
+    }
+    if (status & OC_CLOUD_LOGGED_OUT) {
+        PRINT("\t\t-Logged Out\n");
+    }
+    if (status & OC_CLOUD_DEREGISTERED) {
+        PRINT("\t\t-DeRegistered\n");
+    }
+    if (status & OC_CLOUD_REFRESHED_TOKEN) {
+        PRINT("\t\t-Refreshed Token\n");
+    }
+}
+
+static int
+app_init(void)
+{
+    int ret = oc_init_platform(manufacturer, NULL, NULL);
+    ret |= oc_add_device("/oic/d", device_rt, device_name, spec_version,
+                         data_model_version, NULL, NULL);
+    return ret;
+}
+
+struct switch_t
+{
+    bool state;
+};
+
+struct switch_t switch1 = { 0 };
+
+static void
+get_handler(oc_request_t *request, oc_interface_mask_t iface, void *user_data)
+{
+    struct switch_t *light = (struct switch_t *)user_data;
+
+    PRINT("get_handler:\n");
+
+    oc_rep_start_root_object();
+    switch (iface) {
+        case OC_IF_BASELINE:
+            oc_process_baseline_interface(request->resource);
+            /* fall through */
+        case OC_IF_RW:
+            oc_rep_set_boolean(root, state, light->state);
+            oc_rep_set_text_string(root, name, "Light");
+            break;
+        default:
+            break;
+    }
+    oc_rep_end_root_object();
+    oc_send_response(request, OC_STATUS_OK);
+}
+
+static void
+post_handler(oc_request_t *request, oc_interface_mask_t iface_mask,
+             void *user_data)
+{
+    struct switch_t *light = (struct switch_t *)user_data;
+    (void)iface_mask;
+    printf("post_handler:\n");
+    oc_rep_t *rep = request->request_payload;
+    while (rep != NULL) {
+        char *key = oc_string(rep->name);
+        printf("key: %s ", key);
+        if (key && !strcmp(key, "state")) {
+            switch (rep->type) {
+                case OC_REP_BOOL:
+                    light->state = rep->value.boolean;
+                    PRINT("value: %d\n", light->state);
+                    break;
+                default:
+                    oc_send_response(request, OC_STATUS_BAD_REQUEST);
+                    return;
+            }
+        }
+        rep = rep->next;
+    }
+    oc_send_response(request, OC_STATUS_CHANGED);
+}
+
+static void
+register_resources(void)
+{
+    res1 = oc_new_resource(NULL, "/switch/1", 1, 0);
+    oc_resource_bind_resource_type(res1, resource_rt);
+    oc_resource_bind_resource_interface(res1, OC_IF_RW);
+    oc_resource_set_default_interface(res1, OC_IF_RW);
+    oc_resource_set_discoverable(res1, true);
+    oc_resource_set_observable(res1, true);
+    oc_resource_set_request_handler(res1, OC_GET, get_handler, &switch1);
+    oc_resource_set_request_handler(res1, OC_POST, post_handler, &switch1);
+    oc_cloud_add_resource(res1);
+    oc_add_resource(res1);
+}
+
+int
+main(void)
+{
+    int ret = init();
+    if (ret < 0) {
+        return ret;
+    }
+
+    static const oc_handler_t handler = { .init = app_init,
+            .signal_event_loop = signal_event_loop,
+            .register_resources =
+            register_resources };
+    oc_storage_config("./cloud_server_creds/");
+
+    ret = oc_main_init(&handler);
+    if (ret < 0)
+        return ret;
+
+    oc_cloud_context_t *ctx = oc_cloud_get_context(0);
+    if (ctx) {
+        oc_cloud_manager_start(ctx, cloud_status_handler, NULL);
+    }
+
+    run();
+
+    oc_cloud_manager_stop(ctx);
     oc_main_shutdown();
     return 0;
 }
